5294bb9000dcfefae13392ab80fb16c6ee9686a3
DishaBansal15 <139130914+DishaBansal15@users.noreply.github.com>
2025-08-20 15:31:01 +0530

    Update authUser.js

--- backend/middleware/authUser.js
+++ backend/middleware/authUser.js
@@ -1,23 +1,31 @@
-import { User } from "../models/usermodel.js";
 import jwt from "jsonwebtoken";
+import { User } from "../models/usermodel.js";
 
-//Authentication
 export const isAuthenticated = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
-    console.log("Middleware : ", token);
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+      token = req.headers.authorization.split(" ")[1];
+    } 
+    else if (req.cookies.jwt) {
+      token = req.cookies.jwt;
+    }
+
     if (!token) {
       return res.status(401).json({ error: "User not authenticated" });
     }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
     const user = await User.findById(decoded.userId);
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
+
     req.user = user;
     next();
   } catch (error) {
-    console.log("Error occuring in Authentication: " + error);
+    console.log("Error in Authentication Middleware:", error.message);
     return res.status(401).json({ error: "User not authenticated" });
   }
 };
